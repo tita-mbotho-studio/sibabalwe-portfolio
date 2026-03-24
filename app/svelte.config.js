11fa7363b0b4dab87033e0235e4dd8931ea47d29
tita-mbotho-studio <tita.mbotho.clan@gmail.com>
2026-03-24 00:16:55 +0000

    ENH: Direct path

--- app/svelte.config.js
+++ app/svelte.config.js
@@ -1,6 +1,8 @@
 import adapter from '@sveltejs/adapter-static';
 import { vitePreprocess } from '@sveltejs/vite-plugin-svelte';
 
+const dev = process.argv.includes('dev');
+
 /** @type {import('@sveltejs/kit').Config} */
 const config = {
   preprocess: vitePreprocess(),
@@ -8,7 +10,10 @@ const config = {
     adapter: adapter({
       pages: 'build',
       assets: 'build'
-    })
+    }),
+    paths: {
+      base: dev ? '' : (process.env.BASE_PATH || '')
+    }
   }
 };
 
